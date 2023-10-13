848c62ac2d47c9e2536aa1867d8c2c6a7e6a7a55
Pran Kishore <kishorepran@gmail.com>
2023-10-13 18:12:08 +0530

    Check IPA Generation
    Update the build and test script to generate IPA

--- build-docc.sh
+++ build-docc.sh
@@ -12,3 +12,5 @@ xcrun docc process-archive transform-for-static-hosting \
 
 echo '<script>window.location.href += "/documentation/samplejokes"</script>' > .docs/index.html
 
+#openssl base64 -in AppleDevelopmentKauai.p12 -A | tr -d '\n' > AppleDevelopmentKauai_base64.txt
+#openssl base64 -in Universal_development.mobileprovision -A | tr -d '\n' > Universal_development_base64.txt
