bce6378a293b20ac3812c6c3392f4e076ab1997f
Pran Kishore <kishorepran@gmail.com>
2023-10-14 21:31:12 +0530

    Update build-docc.sh

--- build-docc.sh
+++ build-docc.sh
@@ -12,5 +12,9 @@ xcrun docc process-archive transform-for-static-hosting \
 
 echo '<script>window.location.href += "/documentation/samplejokes"</script>' > .docs/index.html
 
+# Encode
 #openssl base64 -in AppleDevelopmentKauai.p12 -A | tr -d '\n' > AppleDevelopmentKauai_base64.txt
 #openssl base64 -in Universal_development.mobileprovision -A | tr -d '\n' > Universal_development_base64.txt
+
+# Decode
+#openssl base64 -A -d -in certificate_base64.txt -out certificate.p12
